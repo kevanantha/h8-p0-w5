2cbc28219c42143f894e259f17eef1436503b9c5
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-14 11:18:55 +0700

    :rocket:

--- totalDigitRekursif.js
+++ totalDigitRekursif.js
@@ -3,13 +3,13 @@ function totalDigitRekursif(angka) {
   let sum = angkaString % 10
 
   if (!angka) return 0
-  angkaString = angkaString.slice(0, -1);
-  return sum += totalDigitRekursif(angkaString)
+  angkaString = angkaString.slice(0, -1)
+  return (sum += totalDigitRekursif(angkaString))
 }
 
 // TEST CASES
 console.log(totalDigitRekursif(512)) // 8
-console.log(totalDigitRekursif(1542)); // 12
-console.log(totalDigitRekursif(5)); // 5
-console.log(totalDigitRekursif(21)); // 3
-console.log(totalDigitRekursif(11111)); // 5
+console.log(totalDigitRekursif(1542)) // 12
+console.log(totalDigitRekursif(5)) // 5
+console.log(totalDigitRekursif(21)) // 3
+console.log(totalDigitRekursif(11111)) // 5
