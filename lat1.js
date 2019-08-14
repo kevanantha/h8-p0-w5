05e179d4da3071cbc3eb2a8b0f9ccac05122f151
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-14 14:04:44 +0700

    :rocket:

---
+++ lat1.js
@@ -0,0 +1,95 @@
+// 1. memfilter nilai object berdasarkan kondisi tertentu
+
+var exercise1 = [
+  { name: 'ginta', position: 'BF', ability: 'fat', score: 20 },
+  { name: 'hiruma', position: 'DB', ability: 'manipulation', score: 100 },
+  { name: 'eyeshield', position: 'RB', ability: 'topSpeed', score: 80 }
+]
+console.log(exercise1)
+
+// Release 1 => coba ambil nama-nama yang nilainya diatas 30
+function score30(exercise1) {
+  const result = []
+  for (let i = 0; i < exercise1.length; i++) {
+    const { score } = exercise1[i]
+    if (score > 30) {
+      result.push(exercise1[i])
+    }
+  }
+  return result
+}
+console.log(score30(exercise1))
+/* 
+  [ 
+    { name: 'hiruma', position: 'DB', ability: 'manipulation', score: 100},
+    { name: 'eyeshield', position: 'RB', ability: 'topSpeed', score: 80 }
+  ]
+*/
+// Release 2 => coba ambil nama-nama yang nilainya diantara 70 dan 90
+function score7090(exercise1) {
+  const result = []
+  for (let i = 0; i < exercise1.length; i++) {
+    const { score } = exercise1[i]
+    if (score >= 70 && score <= 90) {
+      result.push(exercise1[i])
+    }
+  }
+  return result
+}
+console.log(score7090(exercise1))
+/* 
+  [ 
+    { name: 'eyeshield', position: 'RB', ability: 'topSpeed', score: 80 }
+  ]
+*/
+
+// 2. mensorting array of object berdasarkan value dari objectnya
+
+// Release 1 => sorting isi ascending berdasarkan score
+function sortingScoreAscending(exercise1) {
+  let isSorted = false
+  while (!isSorted) {
+    isSorted = true
+    for (let i = 1; i < exercise1.length; i++) {
+      if (exercise1[i - 1].score > exercise1[i].score) {
+        isSorted = false
+        const temp = exercise1[i - 1]
+        exercise1[i - 1] = exercise1[i]
+        exercise1[i] = temp
+      }
+    }
+  }
+  return exercise1
+}
+console.log(sortingScoreAscending(exercise1))
+/* 
+  [ 
+    { name: 'ginta', position: 'BF', ability: 'fat', score: 20 },
+    { name: 'eyeshield', position: 'RB', ability: 'topSpeed', score: 80 },
+    { name: 'hiruma', position: 'DB', ability: 'manipulation', score: 100}
+  ]
+*/
+// Release 2 => sorting isi descending
+function sortingScoreDescending(exercise1) {
+  let isSorted = false
+  while (!isSorted) {
+    isSorted = true
+    for (let i = 1; i < exercise1.length; i++) {
+      if (exercise1[i - 1].score < exercise1[i].score) {
+        isSorted = false
+        const temp = exercise1[i - 1]
+        exercise1[i - 1] = exercise1[i]
+        exercise1[i] = temp
+      }
+    }
+  }
+  return exercise1
+}
+console.log(sortingScoreDescending(exercise1))
+/* 
+  [ 
+    { name: 'hiruma', position: 'DB', ability: 'manipulation', score: 100},
+    { name: 'eyeshield', position: 'RB', ability: 'topSpeed', score: 80 },
+    { name: 'ginta', position: 'BF', ability: 'fat', score: 20 }
+  ]
+*/
---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var paragraph =
+  'far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts!. Separated they! live in far away from Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small . When she reached the first hills! of the Italic Mountains,she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road!, the Line Lane. Pityful a rethoric question ran over her cheek!'
+
+console.log(paragraph.match(/[^\w]/g))
